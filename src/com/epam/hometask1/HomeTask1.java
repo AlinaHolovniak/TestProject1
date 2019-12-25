9ac9474c3b7050ba0eda01693ed70ed668485ea4
Alina Holovniak <golovniakalina@gmail.com>
2019-12-25 12:48:09 +0200

    Initial commit

---
+++ src/com/epam/hometask1/HomeTask1.java
@@ -0,0 +1,11 @@
+package com.epam.hometask1;
+
+public class HomeTask1 {
+    public static void main(String[] args) {
+        System.out.println("* * * *  * * * *      *      *       *");
+        System.out.println("*        *     *     * *     * *   * *");
+        System.out.println("* *      * * * *    *   *    *   *   *");
+        System.out.println("*        *         * * * *   *       *");
+        System.out.println("* * * *  *        *       *  *       *");
+    }
+}
